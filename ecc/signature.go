aa8a119963824f1877cf514e3d3bdae5d9a933d1
Stefan Alfbo <stefan@alfbo.se>
2024-12-28 13:41:27 +0100

    Add a function to format the signature according to Distinguished Encoding Rules (DER)

--- ecc/signature.go
+++ ecc/signature.go
@@ -17,3 +17,26 @@ func (s *Signature) String() string {
 func NewSignature(r, s *big.Int) *Signature {
 	return &Signature{r, s}
 }
+
+// DER returns the signature in Distinguished Encoding Rules (DER) format
+func (signature *Signature) DER() []byte {
+	r := signature.R.Bytes()
+
+	// If the high bit is set in r, prepend a 0x00 byte
+	if r[0]&0x80 != 0 {
+		r = append([]byte{0x00}, r...)
+	}
+
+	result := append([]byte{0x02, byte(len(r))}, r...)
+
+	s := signature.S.Bytes()
+	// If the high bit is set in s, prepend a 0x00 byte
+	if s[0]&0x80 != 0 {
+		s = append([]byte{0x00}, s...)
+	}
+
+	result = append(result, 0x02, byte(len(s)))
+	result = append(result, s...)
+
+	return append([]byte{0x30, byte(len(result))}, result...)
+}
--- ecc/signature_test.go
+++ ecc/signature_test.go
@@ -1,6 +1,7 @@
 package ecc_test
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
@@ -8,10 +9,9 @@ import (
 )
 
 func TestSignature(t *testing.T) {
-	r := big.NewInt(12345)
-	s := big.NewInt(67890)
-
 	t.Run("NewSignature", func(t *testing.T) {
+		r := big.NewInt(12345)
+		s := big.NewInt(67890)
 		sig := ecc.NewSignature(r, s)
 
 		if sig == nil {
@@ -27,11 +27,29 @@ func TestSignature(t *testing.T) {
 	})
 
 	t.Run("String", func(t *testing.T) {
+		r := big.NewInt(12345)
+		s := big.NewInt(67890)
 		sig := ecc.NewSignature(r, s)
 		expected := "Signature(3039, 10932)"
 
 		if sig.String() != expected {
 			t.Errorf("String: got %v, expected %v", sig.String(), expected)
+		}
+	})
+
+	t.Run("DER", func(t *testing.T) {
+		expected := "3045022037206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c60221008ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec"
+		r := new(big.Int)
+		r.SetString("37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6", 16)
+		s := new(big.Int)
+		s.SetString("8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec", 16)
+		signature := ecc.NewSignature(r, s)
+
+		der := signature.DER()
+		derHex := fmt.Sprintf("%x", der)
+
+		if derHex != expected {
+			t.Errorf("DER: got %s, expected %s", derHex, expected)
 		}
 	})
 }
