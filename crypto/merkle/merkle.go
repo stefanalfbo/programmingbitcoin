b5fa403c92964b28e5dc16e499a09ba9ee59b4c3
Stefan Alfbo <stefan@alfbo.se>
2025-01-28 20:09:35 +0100

    Write the merkle root function

--- crypto/merkle/merkle.go
+++ crypto/merkle/merkle.go
@@ -2,12 +2,14 @@ package merkle
 
 import "github.com/stefanalfbo/programmingbitcoin/crypto/hash"
 
+// Takes the binary hashes and calculates the hash256
 func Parent(left []byte, right []byte) []byte {
 	concatenated := append(left, right...)
 
 	return hash.Hash256(concatenated)
 }
 
+// Takes a list of binary hashes and returns a list that's half the length
 func ParentLevel(level [][]byte) [][]byte {
 	// if the number of hashes is odd, duplicate the last item
 	if len(level)%2 != 0 {
@@ -21,4 +23,15 @@ func ParentLevel(level [][]byte) [][]byte {
 	}
 
 	return parentLevel
+}
+
+// Takes a list of binary hashes and returns the merkle root
+func Root(data [][]byte) []byte {
+	level := data
+
+	for len(level) > 1 {
+		level = ParentLevel(level)
+	}
+
+	return level[0]
 }
--- crypto/merkle/merkle_test.go
+++ crypto/merkle/merkle_test.go
@@ -42,3 +42,27 @@ func TestParentLevel(t *testing.T) {
 		}
 	}
 }
+
+func TestRoot(t *testing.T) {
+	expectedRoot := "acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a5085c4e86c86beed6"
+	hashes := make([][]byte, 12)
+	hashes[0], _ = hex.DecodeString("c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5")
+	hashes[1], _ = hex.DecodeString("c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5")
+	hashes[2], _ = hex.DecodeString("f391da6ecfeed1814efae39e7fcb3838ae0b02c02ae7d0a5848a66947c0727b0")
+	hashes[3], _ = hex.DecodeString("3d238a92a94532b946c90e19c49351c763696cff3db400485b813aecb8a13181")
+	hashes[4], _ = hex.DecodeString("10092f2633be5f3ce349bf9ddbde36caa3dd10dfa0ec8106bce23acbff637dae")
+	hashes[5], _ = hex.DecodeString("7d37b3d54fa6a64869084bfd2e831309118b9e833610e6228adacdbd1b4ba161")
+	hashes[6], _ = hex.DecodeString("8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc")
+	hashes[7], _ = hex.DecodeString("dff6879848c2c9b62fe652720b8df5272093acfaa45a43cdb3696fe2466a3877")
+	hashes[8], _ = hex.DecodeString("b825c0745f46ac58f7d3759e6dc535a1fec7820377f24d4c2c6ad2cc55c0cb59")
+	hashes[9], _ = hex.DecodeString("95513952a04bd8992721e9b7e2937f1c04ba31e0469fbe615a78197f68f52b7c")
+	hashes[10], _ = hex.DecodeString("2e6d722e5e4dbdf2447ddecc9f7dabb8e299bae921c99ad5b0184cd9eb8e5908")
+	hashes[11], _ = hex.DecodeString("b13a750047bc0bdceb2473e5fe488c2596d7a7124b4e716fdd29b046ef99bbf0")
+
+	root := merkle.Root(hashes)
+
+	rootHex := hex.EncodeToString(root)
+	if rootHex != expectedRoot {
+		t.Errorf("Root was incorrect, got: %s, want: %s.", rootHex, expectedRoot)
+	}
+}
