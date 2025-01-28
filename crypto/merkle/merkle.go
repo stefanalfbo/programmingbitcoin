ff4660f620f08435f69b495ee2bb817c40f325c2
Stefan Alfbo <stefan@alfbo.se>
2025-01-28 21:10:41 +0100

    Create a MerkleTree type with String() function

--- crypto/merkle/merkle.go
+++ crypto/merkle/merkle.go
@@ -1,6 +1,12 @@
 package merkle
 
-import "github.com/stefanalfbo/programmingbitcoin/crypto/hash"
+import (
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/stefanalfbo/programmingbitcoin/crypto/hash"
+)
 
 // Takes the binary hashes and calculates the hash256
 func Parent(left []byte, right []byte) []byte {
@@ -34,4 +40,55 @@ func Root(data [][]byte) []byte {
 	}
 
 	return level[0]
+}
+
+type MerkleTree struct {
+	total        int
+	maxDepth     int
+	Nodes        [][][]byte
+	currentDepth int
+	currentIndex int
+}
+
+func NewMerkleTree(total int) *MerkleTree {
+	mt := MerkleTree{total: total}
+	mt.maxDepth = int(math.Ceil(math.Log2(float64(total))))
+
+	for depth := 0; depth < (mt.maxDepth + 1); depth++ {
+		numItems := int32(math.Ceil(float64(mt.total) / math.Pow(2, float64(mt.maxDepth-depth))))
+
+		levelHashes := make([][]byte, numItems)
+
+		mt.Nodes = append(mt.Nodes, levelHashes)
+	}
+
+	mt.currentDepth = 0
+	mt.currentIndex = 0
+
+	return &mt
+}
+
+func (mt *MerkleTree) String() string {
+	result := make([]string, len(mt.Nodes))
+
+	for depth, level := range mt.Nodes {
+		var short string
+		var items []string
+		for index, h := range level {
+			if h == nil {
+				short = "nil"
+			} else {
+				hex := fmt.Sprintf("%x", h)
+				short = fmt.Sprintf("%s...", hex[:8])
+			}
+
+			if depth == mt.currentDepth && index == mt.currentIndex {
+				items = append(items, fmt.Sprintf("*%s*", short[:8]))
+			} else {
+				items = append(items, short)
+			}
+		}
+		result = append(result, fmt.Sprintf("depth %d: %s", depth, strings.Join(items, " ")))
+	}
+	return strings.Join(result, "\n")
 }
--- crypto/merkle/merkle_test.go
+++ crypto/merkle/merkle_test.go
@@ -2,6 +2,7 @@ package merkle_test
 
 import (
 	"encoding/hex"
+	"strings"
 	"testing"
 
 	"github.com/stefanalfbo/programmingbitcoin/crypto/merkle"
@@ -64,5 +65,43 @@ func TestRoot(t *testing.T) {
 	rootHex := hex.EncodeToString(root)
 	if rootHex != expectedRoot {
 		t.Errorf("Root was incorrect, got: %s, want: %s.", rootHex, expectedRoot)
+	}
+}
+
+func TestString(t *testing.T) {
+	hashes := make([][]byte, 16)
+	hashes[0], _ = hex.DecodeString("9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb")
+	hashes[1], _ = hex.DecodeString("5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b")
+	hashes[2], _ = hex.DecodeString("82a02ecbb6623b4274dfcab82b336dc017a27136e08521091e443e62582e8f05")
+	hashes[3], _ = hex.DecodeString("507ccae5ed9b340363a0e6d765af148be9cb1c8766ccc922f83e4ae681658308")
+	hashes[4], _ = hex.DecodeString("a7a4aec28e7162e1e9ef33dfa30f0bc0526e6cf4b11a576f6c5de58593898330")
+	hashes[5], _ = hex.DecodeString("bb6267664bd833fd9fc82582853ab144fece26b7a8a5bf328f8a059445b59add")
+	hashes[6], _ = hex.DecodeString("ea6d7ac1ee77fbacee58fc717b990c4fcccf1b19af43103c090f601677fd8836")
+	hashes[7], _ = hex.DecodeString("457743861de496c429912558a106b810b0507975a49773228aa788df40730d41")
+	hashes[8], _ = hex.DecodeString("7688029288efc9e9a0011c960a6ed9e5466581abf3e3a6c26ee317461add619a")
+	hashes[9], _ = hex.DecodeString("b1ae7f15836cb2286cdd4e2c37bf9bb7da0a2846d06867a429f654b2e7f383c9")
+	hashes[10], _ = hex.DecodeString("9b74f89fa3f93e71ff2c241f32945d877281a6a50a6bf94adac002980aafe5ab")
+	hashes[11], _ = hex.DecodeString("b3a92b5b255019bdaf754875633c2de9fec2ab03e6b8ce669d07cb5b18804638")
+	hashes[12], _ = hex.DecodeString("b5c0b915312b9bdaedd2b86aa2d0f8feffc73a2d37668fd9010179261e25e263")
+	hashes[13], _ = hex.DecodeString("c9d52c5cb1e557b92c84c52e7c4bfbce859408bedffc8a5560fd6e35e10b8800")
+	hashes[14], _ = hex.DecodeString("c555bc5fc3bc096df0a0c9532f07640bfb76bfe4fc1ace214b8b228a1297a4c2")
+	hashes[15], _ = hex.DecodeString("f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e")
+
+	tree := merkle.NewMerkleTree(len(hashes))
+	tree.Nodes[4] = make([][]byte, len(hashes))
+
+	copy(tree.Nodes[4], hashes)
+
+	tree.Nodes[3] = merkle.ParentLevel(tree.Nodes[4])
+	tree.Nodes[2] = merkle.ParentLevel(tree.Nodes[3])
+	tree.Nodes[1] = merkle.ParentLevel(tree.Nodes[2])
+	tree.Nodes[0] = merkle.ParentLevel(tree.Nodes[1])
+
+	expected := "depth 0: *597c4baf*"
+
+	result := tree.String()
+
+	if strings.Contains(result, expected) == false {
+		t.Errorf("String was incorrect, got: %s, want: %s.", result, expected)
 	}
 }
