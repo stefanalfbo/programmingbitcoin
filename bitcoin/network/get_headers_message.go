be830cb2aabe2f4d1ea82772ec7dd66d2c64cf7c
Stefan Alfbo <stefan@alfbo.se>
2025-02-03 19:59:48 +0100

    Refactor the getheaders packet to comply with the types defined in the protocol documentation

--- bitcoin/network/get_headers_message.go
+++ bitcoin/network/get_headers_message.go
@@ -9,18 +9,28 @@ import (
 	"github.com/stefanalfbo/programmingbitcoin/encoding/varint"
 )
 
+// Return a headers packet containing the headers of blocks starting right
+// after the last known hash in the block locator object, up to hash_stop or
+// 2000 blocks, whichever comes first. To receive the next block headers, one
+// needs to issue getheaders again with a new block locator object. Keep in
+// mind that some clients may provide headers of blocks which are invalid if
+// the block locator object contains a hash on the invalid branch.
 type GetHeadersMessage struct {
-	command    []byte
-	version    uint32
-	numHashes  uint8
-	startBlock []byte
-	endBlock   []byte
+	command []byte
+	// The protocol version
+	version uint32
+	// Number of block locator hash entries
+	hashCount uint64
+	// Block locator object; newest back to genesis block (dense to start, but then sparse)
+	startBlock [32]byte
+	// Hash of the last desired block header; set to zero to get as many blocks as possible (2000)
+	endBlock [32]byte
 }
 
-func NewGetHeadersMessage(version uint32, numHashes uint8, startBlock []byte, endBlock []byte) *GetHeadersMessage {
+func NewGetHeadersMessage(version uint32, hashCount uint64, startBlock [32]byte, endBlock [32]byte) *GetHeadersMessage {
 	command := []byte("getheaders")
 
-	return &GetHeadersMessage{command, version, numHashes, startBlock, endBlock}
+	return &GetHeadersMessage{command, version, hashCount, startBlock, endBlock}
 }
 
 func (ghm *GetHeadersMessage) Command() []byte {
@@ -34,19 +44,21 @@ func (ghm *GetHeadersMessage) Serialize() ([]byte, error) {
 	binary.LittleEndian.PutUint32(version, ghm.version)
 	result = append(result, version...)
 
-	numHashes, err := varint.Encode(big.NewInt(int64(ghm.numHashes)))
+	hashCount, err := varint.Encode(big.NewInt(int64(ghm.hashCount)))
 	if err != nil {
 		return nil, err
 	}
-	result = append(result, numHashes...)
+	result = append(result, hashCount...)
 
-	startBlock := make([]byte, 32)
-	slices.Reverse(ghm.startBlock)
-	result = append(result, startBlock...)
+	startBlock := [32]byte{}
+	copy(startBlock[:], ghm.startBlock[:])
+	slices.Reverse(startBlock[:])
+	result = append(result, startBlock[:]...)
 
-	endBlock := make([]byte, 32)
-	slices.Reverse(ghm.endBlock)
-	result = append(result, endBlock...)
+	endBlock := [32]byte{}
+	copy(endBlock[:], ghm.endBlock[:])
+	slices.Reverse(endBlock[:])
+	result = append(result, endBlock[:]...)
 
 	return result, nil
 
