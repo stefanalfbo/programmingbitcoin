68c16040574f457597789aea98786908698cac9c
Stefan Alfbo <stefan@alfbo.se>
2025-03-06 20:08:49 +0100

    Refactor the reject message and add a test when serializing the message

--- bitcoin/network/message/reject_message.go
+++ bitcoin/network/message/reject_message.go
@@ -2,6 +2,8 @@ package message
 
 import (
 	"io"
+
+	"github.com/stefanalfbo/programmingbitcoin/encoding/varint"
 )
 
 type CCode byte
@@ -27,15 +29,15 @@ type RejectMessage struct {
 	// code relating to rejected message
 	CCode CCode
 	// text version of reason for rejection
-	reason string
+	Reason string
 	// Optional extra data provided by some errors. Currently, all errors which
 	// provide this field fill it with the TXID or block header hash of the
 	// object being rejected, so the field is 32 bytes.
-	data []byte
+	Data [32]byte
 }
 
 // NewRejectMessage returns a new RejectMessage
-func NewRejectMessage(message string, ccode CCode, reason string, data []byte) *RejectMessage {
+func NewRejectMessage(message string, ccode CCode, reason string, data [32]byte) *RejectMessage {
 	return &RejectMessage{message, ccode, reason, data}
 }
 
@@ -51,49 +53,35 @@ func (rm *RejectMessage) Command() []byte {
 
 // Serialize serializes the RejectMessage
 func (rm *RejectMessage) Serialize() ([]byte, error) {
-	// Serialize the message
-	messageBytes := []byte(rm.Message)
-	messageLen := len(messageBytes)
-	if messageLen > 12 {
-		messageLen = 12
-	}
-	messageBytes = messageBytes[:messageLen]
+	serialized := make([]byte, 0)
 
-	// Serialize the reason
-	reasonBytes := []byte(rm.reason)
-	reasonLen := len(reasonBytes)
-	if reasonLen > 12 {
-		reasonLen = 12
+	messageLen, err := varint.Encode(uint64(len(rm.Message)))
+	if err != nil {
+		return nil, err
 	}
-	reasonBytes = reasonBytes[:reasonLen]
+	serialized = append(serialized, messageLen...)
+	serialized = append(serialized, []byte(rm.Message)...)
 
-	// Serialize the data
-	dataLen := len(rm.data)
-	if dataLen > 32 {
-		dataLen = 32
+	serialized = append(serialized, byte(rm.CCode))
+
+	reasonLen, err := varint.Encode(uint64(len(rm.Reason)))
+	if err != nil {
+		return nil, err
 	}
-	data := rm.data[:dataLen]
+	serialized = append(serialized, reasonLen...)
+	serialized = append(serialized, []byte(rm.Reason)...)
 
-	// Serialize the reject message
-	serialized := make([]byte, 1+1+1+len(messageBytes)+1+len(reasonBytes)+1+32)
-	serialized[0] = byte(len(messageBytes))
-	serialized[1] = byte(rm.CCode)
-	serialized[2] = byte(len(reasonBytes))
-	copy(serialized[3:], messageBytes)
-	copy(serialized[3+len(messageBytes)+1:], reasonBytes)
-	copy(serialized[3+len(messageBytes)+1+len(reasonBytes)+1:], data)
+	serialized = append(serialized, rm.Data[:]...)
 
 	return serialized, nil
 }
 
 // Parse deserializes the RejectMessage from a reader
 func (rm *RejectMessage) Parse(reader io.Reader) (Message, error) {
-	messageLenBytes := make([]byte, 1)
-	_, err := reader.Read(messageLenBytes)
+	messageLen, err := varint.Decode(reader)
 	if err != nil {
 		return nil, err
 	}
-	messageLen := int(messageLenBytes[0])
 
 	messageBytes := make([]byte, messageLen)
 	_, err = reader.Read(messageBytes)
@@ -111,26 +99,24 @@ func (rm *RejectMessage) Parse(reader io.Reader) (Message, error) {
 	rm.CCode = CCode(ccodeBytes[0])
 
 	// Read the reason
-	reasonLenBytes := make([]byte, 1)
-	_, err = reader.Read(reasonLenBytes)
+	reasonLen, err := varint.Decode(reader)
 	if err != nil {
 		return nil, err
 	}
-	reasonLen := int(reasonLenBytes[0])
 
 	reasonBytes := make([]byte, reasonLen)
 	_, err = reader.Read(reasonBytes)
 	if err != nil {
 		return nil, err
 	}
-	rm.reason = string(reasonBytes)
+	rm.Reason = string(reasonBytes)
 
 	data := make([]byte, 32)
 	_, err = reader.Read(data)
 	if err != nil {
 		return nil, err
 	}
-	rm.data = data
+	copy(rm.Data[:], data[:32])
 
 	return rm, nil
 }
--- bitcoin/network/message/reject_message_test.go
+++ bitcoin/network/message/reject_message_test.go
@@ -21,5 +21,36 @@ func TestParse(t *testing.T) {
 	if rm.CCode != message.REJECT_OBSOLETE {
 		t.Errorf("expected ccode %d, got %d", message.REJECT_OBSOLETE, rm.CCode)
 	}
+}
 
+func TestSerializeAndParseRejectMessage(t *testing.T) {
+	var data [32]byte
+	copy(data[:], []byte("orphan block"))
+	rm := message.NewRejectMessage("command", message.REJECT_OBSOLETE, "reason", data)
+
+	expected, _ := hex.DecodeString("07636f6d6d616e641106726561736f6e6f727068616e20626c6f636b0000000000000000000000000000000000000000")
+	serialized, _ := rm.Serialize()
+
+	if !bytes.Equal(serialized, expected) {
+		t.Errorf("expected %x, got %x", expected, serialized)
+	}
+
+	rmParsed := message.NewEmptyRejectMessage()
+	rmParsed.Parse(bytes.NewReader(serialized))
+
+	if rm.Message != rmParsed.Message {
+		t.Errorf("expected %s, got %s", rm.Message, rmParsed.Message)
+	}
+
+	if rm.CCode != rmParsed.CCode {
+		t.Errorf("expected %d, got %d", rm.CCode, rmParsed.CCode)
+	}
+
+	if rm.Reason != rmParsed.Reason {
+		t.Errorf("expected %s, got %s", rm.Reason, rmParsed.Reason)
+	}
+
+	if !bytes.Equal(rm.Data[:], rmParsed.Data[:]) {
+		t.Errorf("expected %x, got %x", rm.Data, rmParsed.Data)
+	}
 }
